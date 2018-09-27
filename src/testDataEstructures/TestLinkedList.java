92a5b64f12fc85a7536eeeacd1e5568d80476dce
LuisAlfredo <luisalfredo9905@gmail.com>
2018-09-27 10:19:01 -0500

    Making real test of the data structures

---
+++ src/testDataEstructures/TestLinkedList.java
@@ -0,0 +1,14 @@
+package testDataEstructures;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestLinkedList {
+	
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
